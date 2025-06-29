8442a68a67eeb1dea92df27b59d2f9fe72b69b4b
Brayan Sánchez <brondevc0n@proton.me>
2025-06-28 23:54:59 -0400

    Update capitulo1.py
    add new lines

--- Python#-1/capitulo1.py
+++ Python#-1/capitulo1.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python
 
+# primeras declaraciones de python3 ^ 3.12 ^ 3.15 
 
+# imprimir
 print('hello program')
 
+# metodos cortos de autorespuestas 
+
+print(f'primera forma \033[m')
+
+#primera parte
+
+#camelCase 
+Avar = "1"
+
+1valor = 0
+
+V__c = 22
+
+# variables formato hungaro
+
+Flush = valor 
+
+sy_Supper = sy_Supper 
+
+def funcA(var_c) 
+  return  0+1
+print(var_c)
