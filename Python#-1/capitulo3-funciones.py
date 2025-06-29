4db3f7f5a66aa4b898d16dc2c58fe412336b1d11
Brayan Sánchez <brondevc0n@proton.me>
2025-06-28 23:59:24 -0400

    Create capitulo3-funciones.py
    funciones v1 - 28-06-25

---
+++ Python#-1/capitulo3-funciones.py
@@ -0,0 +1,17 @@
+#primeros pasos 
+
+# basica 
+
+def funcionbasica ():
+  pass
+
+print(funcionbasica)
+
+# funciones metodos acciones
+def Pyfunc (variable1 , variable2 , variable3):
+  for x in range(1,10):
+    variable1 = variable1 + x
+    variable2 = variable2 - x
+    variable3 = variable3 + x
+
+print(Pyfunc(1,0,3))
