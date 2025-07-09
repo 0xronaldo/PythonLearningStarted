7f6596e031ae35dce77f514bfa0e41e84222274b
0xronaldo <brondevc0n@proto.me>
2025-07-09 00:06:02 -0400

    add python files

---
+++ Python#-2/var_recursives.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def main():
+	nombre="Entero"
+	nombre_a="Double"
+	nombre_b="Float"
+
+	print(nombre, nombre_a , nombre_b)
+
+print("verificador de valores")
+
+
+
+
+if __name__ == "__main__":
+	main()
+	print(\n)
