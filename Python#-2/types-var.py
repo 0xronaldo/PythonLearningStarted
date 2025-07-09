4b048caf13d5583f48faec3b04368ac982b2cd71
0xronaldo <brondevc0n@proto.me>
2025-07-08 23:59:30 -0400

    add files p

---
+++ Python#-2/types-var.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+# tipos de variables en python 
+
+
+print("hola_python!!")
+print("hola_python!!")
+print("hola_python!!")
+
+
+variable1 = "texto"
+
+print(variable1)
+
+
+# enteros 
+
+var_int = 5
+var_double = 78.2
+
+var_float = 45.0002
+
+
+print(var_int)
+print(var_double)
+print(var_float)
